4f0913bd6f9dd740862f7e46b7ad2a7da635619b
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-21 17:48:42 +0300

    store yaratildi. index.js'de import edildi. app provider ile sarildi ve icerisinde store yaratilip eklendi.

--- src/index.js
+++ src/index.js
@@ -9,12 +9,15 @@ import './index.css';
 import { legacy_createStore as createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import {store} from "./store/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store = {store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
