47f3083884400455ce25e4dfaa6945e66a9b4587
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-21 17:59:11 +0300

    useSelector ile appTitle'a eristik.

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const AppHeader = (props) => {
-  const appTitle = "";
+  const appTitle = useSelector((store) => store.appTitle);
   const displayFavorites = true;
 
   return (
