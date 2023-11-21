b62fd845c48f38fea3804daf4baaa332dab09455
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-21 19:26:31 +0300

    Favoriler reduceri olusturuldu. index.js dosyasına import edildi. combineReducers metodu kullanildi.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
-
+import favoriteReducer from "./favoriteReducer";
 import movieReducer from './movieReducer';
 
-export default movieReducer;
+const rootReducer = combineReducers({
+    movieReducer, 
+    favoriteReducer
+})
+
+export default rootReducer;
