ed578a6cab3074483c1b789ce0ed6c02d05af5af
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-21 21:11:27 +0300

    AddFavorite reducer'i guncellendi. (2 kez ayni film fav. eklenmiyor)

--- src/reducers/favoriteReducer.js
+++ src/reducers/favoriteReducer.js
@@ -1,4 +1,8 @@
-import{TOGGLE_FAVORITES, ADD_FAVORITE, REMOVE_FAVORITE} from "../actions/favoritesActions"
+import {
+  TOGGLE_FAVORITES,
+  ADD_FAVORITE,
+  REMOVE_FAVORITE,
+} from "../actions/favoritesActions";
 
 const initialState = {
   favorites: [],
@@ -6,28 +10,36 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-      case TOGGLE_FAVORITES:
-        return {
-          ...state,
-          displayFavorites: !state.displayFavorites,
-        };
-        case ADD_FAVORITE:
+  switch (action.type) {
+    case TOGGLE_FAVORITES:
+      return {
+        ...state,
+        displayFavorites: !state.displayFavorites,
+      };
+    case ADD_FAVORITE:
+      const oldMovie = state.favorites.find(
+        (movie) => movie.id === action.payload.id
+      );
+      if (oldMovie) {
+        return state;
+      } else {
         return {
           ...state,
           favorites: [...state.favorites, action.payload],
         };
-        case REMOVE_FAVORITE:
-        return {
-          ...state,
-          favorites: state.favorites.filter(movies => {
-            return movies.id !== action.payload
-          })
-        };
-        
-        default:
-            return state;
-    }
+      }
+
+    case REMOVE_FAVORITE:
+      return {
+        ...state,
+        favorites: state.favorites.filter((movies) => {
+          return movies.id !== action.payload;
+        }),
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;
